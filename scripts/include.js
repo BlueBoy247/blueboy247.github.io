978ac9d2224b64a36d55c32c83351f9bb13d1d0c
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2023-04-27 18:52:28 +0800

    modified <head><header><footer>

--- scripts/include.js
+++ scripts/include.js
@@ -1,13 +1,11 @@
 $(function(){
-    $.get('/public/head.html', function(data) {
-      $('head').append(data);
-    });
-    $.get('/public/header.html', function(data) {
-      data = data.replace(/>[\n\t ]+</g, "><");
-      $('header').append(data);
-    });
-    $.get('/public/footer.html', function(data) {
-      data = data.replace(/>[\n\t ]+</g, "><");
-      $('footer').append(data);
-    });
+  $.get('/public/head.html', function(data) {
+    $('head').append(data);
+  });
+  $(document).ready(function() {
+    $('header').load('/public/header.html');
+  });
+  $(document).ready(function() {
+    $('footer').load('/public/footer.html');
+  });
 });   
