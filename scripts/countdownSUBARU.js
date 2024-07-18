6d92aaeaf0cdcff7632bfd3eb6140be05e644521
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2024-07-18 17:13:40 +0800

    Delete countdownSUBARU.js

--- scripts/countdownSUBARU.js
+++
@@ -1,8 +0,0 @@
-(function(d, s, id) {
-    let js, pjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://www.tickcounter.com/static/js/loader.js";
-    pjs.parentNode.insertBefore(js, pjs);
-}(document, "script", "tickcounter-sdk"));
